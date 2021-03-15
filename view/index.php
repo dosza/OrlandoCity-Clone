a3dc5c1c1645a77193d175d203937a57236c5c00
DanielOliveiraSouza <oliveira.daniel109@gmail.com>
2021-03-15 19:00:40 -0300

    inicializa rotas com o slim

---
+++ view/index.php
@@ -0,0 +1,184 @@
+<?php 
+	include_once('header.php'); 
+?>
+		<section>
+
+			<div id="banner">
+
+				<h1>Orlando City<small>Orlando City Soccer Club</small></h1>
+
+			</div>
+			
+			<div id="news" class="container">
+
+				<div class="row text-center">
+					<h2>Latest News</h2>
+					<hr>
+				</div>
+
+		
+					<button type="button" id="btn-news-prev"><i class="fa fa-angle-left"></i></button>
+					<button type="button" id="btn-news-next"><i class="fa fa-angle-right"></i></button>
+
+				<div class="row thumbnails">
+
+					<div class="item">
+
+						<div class="item-inner">
+
+							<img src="img/noticia-thumb.jpg" alt="Noticia">
+							<h3>Orlando City Acquires Goalkeeper Joe Bendik from Toronto FC</h3>
+							<time>December 21,2015</time>
+
+						</div>
+					</div>
+
+					<div class="item">
+
+						<div class="item-inner">
+
+							<img src="img/noticia-thumb.jpg" alt="Noticia">
+							<h3>Orlando City Acquires Goalkeeper Joe Bendik from Toronto FC</h3>
+							<time>December 21,2015</time>
+						
+						</div>
+
+					</div>
+
+					<div class="item">
+
+						<div class="item-inner">
+
+							<img src="img/noticia-thumb.jpg" alt="Noticia">
+							<h3>Orlando City Acquires Goalkeeper Joe Bendik from Toronto FC</h3>
+							<time>December 21,2015</time>
+
+						</div>
+
+					</div>
+
+					<div class="item">
+
+						<div class="item-inner">
+
+							<img src="img/noticia-thumb.jpg" alt="Noticia">
+							<h3>Orlando City Acquires Goalkeeper Joe Bendik from Toronto FC</h3>
+							<time>December 21,2015</time>
+
+						</div>
+
+					</div>
+
+					<div class="item">
+
+							<div class="item-inner">
+
+								<img src="img/noticia-thumb.jpg" alt="Noticia">
+								<h3>Orlando City Acquires Goalkeeper Joe Bendik from Toronto FC</h3>
+								<time>December 21,2015</time>
+
+							</div>
+					</div>
+
+					<div class="item">
+
+						<div class="item-inner">
+
+							<img src="img/noticia-thumb.jpg" alt="Noticia">
+							<h3>Orlando City Acquires Goalkeeper Joe Bendik from Toronto FC</h3>
+							<time>December 21,2015</time>
+
+						</div>
+
+					</div>
+
+					<div class="item">
+
+						<div class="item-inner">
+
+							<img src="img/noticia-thumb.jpg" alt="Noticia">
+							<h3>Orlando City Acquires Goalkeeper Joe Bendik from Toronto FC</h3>
+							<time>December 21,2015</time>
+
+						</div>
+
+					</div>
+
+					<div class="item">
+
+						<div class="item-inner">
+
+							<img src="img/noticia-thumb.jpg" alt="Noticia">
+							<h3>Orlando City Acquires Goalkeeper Joe Bendik from Toronto FC</h3>
+							<time>December 21,2015</time>
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+			
+			<div id="estatisticas">
+
+				<div class="container">
+
+					<div class="row">
+
+						<div class="col-md-4">
+							<p>61348<small>Capacity</small></p>
+						
+						</div>
+
+						<div class="col-md-4">
+							<p>2010<small>Founded</small></p>
+						</div>
+
+						<div class="col-md-4">
+							<p>7 th<small>Eastern Conference</small></p>
+						</div>
+
+					</div>
+				</div>
+
+			</div>
+
+			<div id="call-to-action">
+				<div class="row text-center">
+					<h2>American club number one in Brazil </h2>
+					<hr>
+				</div>
+
+				<div class="row">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+
+				<div class="row row-max-400">
+					
+					<div class="col-xs-6">
+						<a href="#" class="btn btn-roxo">Shop</a>
+					</div>						
+					<div class="col-xs-6">
+						<a href="#" class="btn btn-amarelo">Register</a>
+					</div>			
+
+				</div>
+			</div>
+
+		</section>
+
+		<!-- Rodapé-->
+		<?php 
+			include_once('footer.php');
+		?>
+	</body>
+
+</html>
