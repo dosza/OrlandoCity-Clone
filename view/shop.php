ef7b93a1b6ccfd49ef1a0a974ff6232510e961f0
Daniel <oliveira.daniel109@gmail.com>
2021-03-16 05:02:20 -0300

    aplica estilização na descrição de compras

--- view/shop.php
+++ view/shop.php
@@ -2,40 +2,40 @@
 		
 		<section>
 
-			<div class="container">
+			<div class="container" id="destaque-produtos-container">
 
-				<div class="destaque-produtos">
+				<div id="destaque-produtos">
 
 					<div class="item">
 
-						<div class="col-sm-6">
+						<div class="col-sm-6 col-imagem">
 							<img src="img/produtos/moto-x.png" alt="Nome do produto">
 							
 						</div>
 
-						<div class="col-sm-6">
+						<div class="col-sm-6 col-descricao">
 							<h2>Smartphone Moto X Play Dual Chip Desbloqueado Android 5.1</h2>	
 
 							<div class="box-valor">
 
-								<div class="text-noboleto">no boleto</div>
-								<div class="text-por">por</div>
-								<div class="text-reais">R$</div>
-								<div class="text-valor">1.259</div>
-								<div class="text-valor-centavos">,10</div>
-								<div class="text-parcelas">ou em até 8x de R$ 174,88</div>
-								<div class="text-total">total a prazo R$ 1399,00</div>
-								<a href="" class="btn btn-comprar"><i class="fa fa-shopping-cart"></i>Compre agora</a>
+								<div class="text-noboleto text-arial-cinza">no boleto</div>
+								<div class="text-por text-arial-cinza">por</div>
+								<div class="text-reais text-roxo">R$</div>
+								<div class="text-valor text-roxo">1.259</div>
+								<div class="text-valor-centavos text-roxo">, 10</div>
+								<div class="text-parcelas text-arial-cinza">ou em até 8x de R$ 174,88</div>
+								<div class="text- text-arial-cinza">total a prazo R$ 1399,00</div>
 							</div>
+							<a href="" class="btn btn-comprar text-roxo"><i class="fa fa-shopping-cart"></i> Compre agora</a>
 
 						</div>
 
 					</div>
 
 				</div>
-				<button id="btn-news-prev"><i class="fa fa-angle-left"></i></button>
-				<button id="btn-news-next"><i class="fa fa-angle-right"></i></button>
 
+				<button id="btn-destaque-prev"><i class="fa fa-angle-left"></i></button>
+				<button id="btn-destaque-next"><i class="fa fa-angle-right"></i></button>
 			</div>
 
 		</section>
