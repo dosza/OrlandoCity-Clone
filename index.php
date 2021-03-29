730e61341597b895ac3a177858a2fa6f909dd421
Daniel <oliveira.daniel109@gmail.com>
2021-03-29 05:09:20 -0300

    formata a renderização dos produtos
    formata a visualização dos dados do produto

--- index.php
+++ index.php
@@ -34,8 +34,21 @@ $app->get('/produtos',
     function(){
 
         $string_query="SELECT nome_prod_longo,foto_principal,preco FROM tb_produtos WHERE preco_promorcional > 0 ORDER BY  preco_promorcional DESC LIMIT 3;";
+
+        /*nome_prod_longo,foto_principal,preco,centavos,parcelas,parcela,total*/
         $sql = new Sql();
         $data = $sql->select($string_query);
+
+
+        foreach ($data as &$produto) {
+            $preco = $produto['preco'];
+            $centavos = explode(".",$preco);
+            $produto['preco'] = number_format($preco,0,",",".");
+            $produto['centavos'] = end($centavos);
+            $produto['parcelas'] = 10;
+            $produto['parcela'] = number_format($preco/$produto['parcelas'],2, ",", ".");
+            $produto['total'] = number_format($preco, 2, ",", ".");
+        }
         echo json_encode($data);
     }
 );
